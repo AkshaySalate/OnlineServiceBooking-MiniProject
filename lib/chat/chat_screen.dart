76334d2bd4b6cc54f9d7eea49e5e936825c3d719
Akshay Salate <salateakshay@gmail.com>
2025-03-19 14:23:30 +0530

    theme for chat page

--- lib/chat/chat_screen.dart
+++ lib/chat/chat_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:online_service_booking/theme.dart';
 
 class ChatScreen extends StatefulWidget {
   final String providerId;
@@ -44,69 +45,84 @@ class _ChatScreenState extends State<ChatScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Chat"),
-        actions: [
-          IconButton(
-            icon: Icon(Icons.delete, color: Colors.white),
-            onPressed: clearChat,
-          ),
-        ],
+      appBar: AppTheme.gradientAppBarWithIcon(
+        "Chat",
+        Icons.delete,
+        Colors.white,
+        clearChat,
       ),
-      body: Column(
-        children: [
-          Expanded(
-            child: StreamBuilder(
-              stream: _firestore
-                  .collection("chats")
-                  .doc(getChatRoomId())
-                  .collection("messages")
-                  .orderBy("timestamp", descending: true)
-                  .snapshots(),
-              builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
-                if (!snapshot.hasData) {
-                  return Center(child: CircularProgressIndicator());
-                }
-                return ListView.builder(
-                  reverse: true,
-                  itemCount: snapshot.data!.docs.length,
-                  itemBuilder: (context, index) {
-                    var message = snapshot.data!.docs[index];
-                    bool isMe = message["sender"] == _auth.currentUser!.uid;
-                    return Align(
-                      alignment: isMe ? Alignment.centerRight : Alignment.centerLeft,
-                      child: Container(
-                        margin: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
-                        padding: EdgeInsets.all(10),
-                        decoration: BoxDecoration(
-                          color: isMe ? Colors.blue : Colors.grey,
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        child: Text(message["message"], style: TextStyle(color: Colors.white)),
-                      ),
-                    );
-                  },
-                );
-              },
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.all(8.0),
-            child: Row(
+      body: Container(
+        decoration: AppTheme.gradientBackground,
+        child: Stack(
+          children: [
+            ...AppTheme.floatingIcons(context),
+            Column(
               children: [
                 Expanded(
-                  child: TextField(
-                    controller: _messageController,
-                    decoration: InputDecoration(labelText: "Type a message"),
+                  child: StreamBuilder(
+                    stream: _firestore
+                        .collection("chats")
+                        .doc(getChatRoomId())
+                        .collection("messages")
+                        .orderBy("timestamp", descending: true)
+                        .snapshots(),
+                    builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+                      if (!snapshot.hasData) {
+                        return Center(child: CircularProgressIndicator());
+                      }
+                      return ListView.builder(
+                        reverse: true,
+                        itemCount: snapshot.data!.docs.length,
+                        itemBuilder: (context, index) {
+                          var message = snapshot.data!.docs[index];
+                          bool isMe = message["sender"] == _auth.currentUser!.uid;
+                          return Align(
+                            alignment: isMe ? Alignment.centerRight : Alignment.centerLeft,
+                            child: Container(
+                              margin: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                              padding: EdgeInsets.all(10),
+                              decoration: BoxDecoration(
+                                color: isMe ? Colors.blue.shade700 : Colors.grey.shade700,
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              child: Text(
+                                message["message"],
+                                style: TextStyle(color: Colors.white, fontSize: 16),
+                              ),
+                            ),
+                          );
+                        },
+                      );
+                    },
                   ),
                 ),
-                IconButton(
-                  icon: Icon(Icons.send, color: Colors.blue),
-                  onPressed: sendMessage,
+                Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: TextField(
+                          controller: _messageController,
+                          decoration: InputDecoration(
+                            hintText: "Type a message",
+                            filled: true,
+                            fillColor: Colors.white.withOpacity(0.8),
+                            border: AppTheme.lightTheme.inputDecorationTheme.enabledBorder,
+                            focusedBorder: AppTheme.lightTheme.inputDecorationTheme.focusedBorder,
+                          ),
+                        ),
+                      ),
+                      IconButton(
+                        icon: Icon(Icons.send, color: Colors.yellow),
+                        onPressed: sendMessage,
+                      ),
+                    ],
+                  ),
                 ),
               ],
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
